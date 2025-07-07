0143956bc15a21453fb05493ab58b4df4e27312c
Godwin-AKAKPO <mouyiwaakakpo@gmail.com>
2025-07-07 17:32:28 +0100

    Animation d'ouverture

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 $(function(){
-    var $mainMenuItems = $("#main-menu ul").children("li"),
+    let $mainMenuItems = $("#main-menu ul").children("li"),
     totalMainMenuItems = $mainMenuItems.length,
-    openedIndex = -1,
+    openedIndex =2,
 
     init = function(){
+       bindEvent();
+       if(validIndex(openedIndex)){
+             animateItem( $mainMenuItems.eq(openedIndex), true, 700);
+       }
+    };
+    bindEvent = function(){
         $mainMenuItems.children(".images").click(function(){
-            var newIndex = $(this).parent().index();
-            var $item = $mainMenuItems.eq(newIndex);
+            let newIndex = $(this).parent().index();
+            let $item = $mainMenuItems.eq(newIndex);
             if(openedIndex=== newIndex)
             {
                 animateItem($item, false, 250);
@@ -22,14 +28,14 @@ $(function(){
                 }
             }
         });
-    };
+    }
 
     validIndex = function(indexToCheck){
         return (indexToCheck >= 0) &&(indexToCheck< totalMainMenuItems);
     }
 
     animateItem = function($item, toOpen, speed){
-        var $colorImage = $item.find(".color");
+        let $colorImage = $item.find(".color");
         itemParam  = toOpen ? {width:"420px"}:{width:"140px"},
         colorImageParam = toOpen ? {left : "0px"} : {left:"140px"};
         $colorImage.animate(colorImageParam,speed);
